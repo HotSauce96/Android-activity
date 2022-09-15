7cd0e661472e48e64f1e200dd1154e7a192e0b50
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2022-09-15 00:03:23 -0500

    Adding files

---
+++ app/src/main/java/com/example/turismoviajes/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.turismoviajes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ciudades, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+    public abstract class SpinnerActivity extends Activity implements NavigationBarView.OnItemSelectedListener {
+
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+
+            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+            spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
+
+}
+
